0015c23d47eebb9d4868f878c774bbdff530a2d8
IBilldThings <200523655+IBilldThings@users.noreply.github.com>
2025-12-29 18:45:42 -0500

    feat: add getHumanChoice code

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,16 +1,23 @@
 function getComputerChoice() {
-    chosen = ""
-    randomValue = Math.random()
+    let compChosen = ""
+    const randomValue = Math.random()
     if (randomValue < 0.33){
-        chosen = "Scissors"
+        compChosen = "scissors"
     };
     if (randomValue > 0.33 && randomValue < 0.66){
-        chosen = "Rock"
+        compChosen = "rock"
     };
     if (randomValue > 0.66){
-        chosen = "Paper"
+        compChosen = "paper"
     };
-    return chosen
+    return compChosen
+}
+
+function getHumanChoice(){
+    userInput = window.prompt("What is your choice?");
+    playerChoice = String(userInput).toLowerCase();
+    return playerChoice
 }
 
 console.log(getComputerChoice());
+console.log(getHumanChoice());
